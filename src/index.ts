42e3efb9af5bf631a168e2a22927e5d74625338b
mchami <chamim@ethz.ch>
2024-06-11 17:24:04 +0200

    toggle works

--- src/index.ts
+++ src/index.ts
@@ -1,158 +1,94 @@
 import {
-  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+
+
+import { ToolbarButton } from '@jupyterlab/apputils';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+
 import {
-  ICommandPalette,
-  MainAreaWidget,
-  WidgetTracker
-} from '@jupyterlab/apputils';
+  NotebookPanel,
+  INotebookModel,
+} from '@jupyterlab/notebook';
 
-import { Widget } from '@lumino/widgets';
+import { ISharedCodeCell } from '@jupyter/ydoc';
+import {IOutput} from '@jupyterlab/nbformat'
+import '../style/index.css';
 
-interface APODResponse {
-  copyright: string;
-  date: string;
-  explanation: string;
-  media_type: 'video' | 'image';
-  title: string;
-  url: string;
-};
 
-class APODWidget extends Widget {
-  /**
-  * Construct a new APOD widget.
-  */
-  constructor() {
-    super();
 
-    this.addClass('my-apodWidget');
-
-    // Add an image element to the panel
-    this.img = document.createElement('img');
-    this.node.appendChild(this.img);
-
-    // Add a summary element to the panel
-    this.summary = document.createElement('p');
-    this.node.appendChild(this.summary);
-  }
-
-  /**
-  * The image element associated with the widget.
-  */
-  readonly img: HTMLImageElement;
-
-  /**
-  * The summary text element associated with the widget.
-  */
-  readonly summary: HTMLParagraphElement;
-
-  /**
-  * Handle update requests for the widget.
-  */
-  async updateAPODImage(): Promise<void> {
-
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`);
-
-    if (!response.ok) {
-      const data = await response.json();
-      if (data.error) {
-        this.summary.innerText = data.error.message;
-      } else {
-        this.summary.innerText = response.statusText;
-      }
-      return;
-    }
-
-    const data = await response.json() as APODResponse;
-
-    if (data.media_type === 'image') {
-      // Populate the image
-      this.img.src = data.url;
-      this.img.title = data.title;
-      this.summary.innerText = data.title;
-      if (data.copyright) {
-        this.summary.innerText += ` (Copyright ${data.copyright})`;
-      }
-    } else {
-      this.summary.innerText = 'Random APOD fetched was not an image.';
-    }
-  }
-
-  /**
-  * Get a random date string in YYYY-MM-DD format.
-  */
-  randomDate(): string {
-    const start = new Date(2010, 1, 1);
-    const end = new Date();
-    const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
-    return randomDate.toISOString().slice(0, 10);
-  }
+interface Code {
+  cluster_id: number;
+  code: string;
+  outputs: IOutput[];
+  code_id: number;
 }
 
 /**
-* Activate the APOD widget extension.
-*/
-function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer | null) {
-  console.log('JupyterLab extension jupyterlab_apod is activated!');
+ * Initialization data for the jupyterlab_hide_code extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_hide_code:plugin',
+  description:
+    'A button in JupyterLab to run the code cells and then to hide the code cells.',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+    console.log('JupyterLab extension jupyterlab_hide_code is activated!');
+  }
+};
 
-  // Declare a widget variable
-  let widget: MainAreaWidget<APODWidget>;
 
-  // Add an application command
-  const command: string = 'apod:open';
-  app.commands.addCommand(command, {
-    label: 'Random Astronomy Picture',
-    execute: () => {
-      if (!widget || widget.isDisposed) {
-        const content = new APODWidget();
-        widget = new MainAreaWidget({content});
-        widget.id = 'apod-jupyterlab';
-        widget.title.label = 'Astronomy Picture';
-        widget.title.closable = true;
+
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+
+    const toggleCode = () => {
+      let currCell = panel.content.activeCell;
+      // NotebookActions.runAll(panel.content, context.sessionContext);
+      if (currCell?.model.type === 'code') {
+        let codes: Code[] = currCell?.model.metadata['codes'] as unknown as Code[];
+        let code_id: number = currCell.model.metadata['code_id'] as unknown as number;
+        codes.forEach((code) => {
+          if (code.code_id != code_id) {
+            currCell?.model.sharedModel.setSource(code.code);
+            currCell?.model.setMetadata('code_id', code.code_id);
+            currCell?.model.setMetadata('cluster_id', code.cluster_id);
+            console.log("New code id is: ", code.code_id);
+            let c = currCell?.model.sharedModel as unknown as ISharedCodeCell;
+            c.setOutputs(code.outputs);
+          }
+        });
+        // currCell.hide();
       }
-      if (!tracker.has(widget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(widget);
-      }
-      if (!widget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.add(widget, 'main');
-      }
-      widget.content.updateAPODImage();
 
-      // Activate the widget
-      app.shell.activateById(widget.id);
-    }
-  });
+      buttonShowInput.show();
+    };
 
-  // Add the command to the palette.
-  palette.addItem({ command, category: 'Tutorial' });
 
-  // Track and restore the widget state
-  let tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
-    namespace: 'apod'
-  });
-  if (restorer) {
-    restorer.restore(tracker, {
-      command,
-      name: () => 'apod'
+    const buttonShowInput = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-sm fa-eye fontawesome-colors',
+      onClick: toggleCode,
+      tooltip: 'Show Input'
+    });
+
+    buttonShowInput.show();
+
+    panel.toolbar.insertItem(11, 'showInput', buttonShowInput);
+
+    return new DisposableDelegate(() => {
+      buttonShowInput.dispose();
     });
   }
 }
-
-/**
- * Initialization data for the jupyterlab_apod extension.
- */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-apod',
-  description: 'Show a random NASA Astronomy Picture of the Day in a JupyterLab panel.',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: activate,
-  optional: [ILayoutRestorer]
-};
 
 export default plugin;
