b85191789a125454527432dbc90f888bb77d2a85
mchami <chamim@ethz.ch>
2024-08-01 18:38:45 +0100

    make extension start automatically and only if has visualization field in metadata

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import {
 
 import { FlowchartWidget } from './graphWidget';
 import { NotebookManager } from './notebookManager';
-import { ICommandPalette } from '@jupyterlab/apputils';
+
 
 import { OpenNotebookButton } from './openNotebookButton';
 import { NotebookSelector } from './notebookSelector';
@@ -27,59 +27,50 @@ import { NotebookSelector } from './notebookSelector';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'sidebar-cluster:plugin',
   autoStart: true,
-  requires: [ICommandPalette, INotebookTracker, IFileBrowserFactory],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette, tracker: INotebookTracker, factory: IFileBrowserFactory) => {
+  requires: [INotebookTracker, IFileBrowserFactory],
+  activate: (app: JupyterFrontEnd, tracker: INotebookTracker, factory: IFileBrowserFactory) => {
 
-    console.log('JupyterLab extension sidebar-cluster is activated!');
-    const notebookManager = new NotebookManager();
-    const graphWidget = new FlowchartWidget(notebookManager);
-    const notebookSelector = new NotebookSelector(notebookManager, graphWidget);
 
     // When a notebook is opened, add the custom widget to the top
-    tracker.widgetAdded.connect((sender, notebook) => {
-      console.log('Notebook added');
-      const toolbar = notebook.toolbar.node;
-      const customWidget = notebookSelector.createSelector();
+    tracker.widgetAdded.connect((sender, panel) => {
+      panel.context.ready.then(() => {
+        
+        panel.content.model?.sharedModel.getMetadata('visualization');
+        if (!panel.content.model?.sharedModel.getMetadata('visualization')){
+          return;
+        }
 
-      // Insert the custom widget at the beginning of the toolbar
-      toolbar.parentNode?.insertBefore(customWidget.node, toolbar.parentNode?.nextSibling as Node);
-    });
+          // Initialize the classes
+          const notebookManager = new NotebookManager();
+          const graphWidget = new FlowchartWidget(notebookManager);
+          const notebookSelector = new NotebookSelector(notebookManager, graphWidget);
 
-
-    app.commands.addCommand('sidebar-cluster:open', {
-      label: 'Generate sidebar graphs',
-      caption: 'Generate sidebar graphs',
-      execute: () => {
-        const panel = tracker.currentWidget;
-        if (panel) {
+          // Instantiating the notebook manager
           notebookManager.populateCells(panel.content.widgets);
-          notebookManager.showAllNotebooks();
-          // Create a new button extension
+          notebookManager.showNotebooks([]);
+
+          // Adding the toolbar selector
+          const toolbar = panel.toolbar.node;
+          const customWidget = notebookSelector.createSelector();
+          toolbar.parentNode?.insertBefore(customWidget.node, toolbar.parentNode?.nextSibling as Node);
+          notebookSelector.addOptions();
+          console.log('Selector added to toolbar');
+
+          // Create the Open Notebooks button
           const buttonExtension = new OpenNotebookButton(notebookManager, factory);
           panel.toolbar.insertItem(11, 'showNotebook', buttonExtension.createButton());
           console.log('Button extension added to notebook');
-        }
-        else {
-          console.log('No notebook is open');
-        }
 
-        graphWidget.id = 'my-sidebar-graph-widget';
-        graphWidget.title.iconClass = 'jp-SideBar-tabIcon';
-        graphWidget.title.caption = 'My Sidebar Graph Widget';
+          // Add the graph widget to the sidebar
+          graphWidget.id = 'my-sidebar-graph-widget';
+          graphWidget.title.iconClass = 'jp-SideBar-tabIcon';
+          graphWidget.title.caption = 'My Sidebar Graph Widget';
+          app.shell.add(graphWidget, 'left');
+          app.shell.activateById(graphWidget.id);
+          console.log('Graph widget added to sidebar');
 
-        // Add the sidebar to the left area
-        app.shell.add(graphWidget, 'left');
-        app.shell.activateById(graphWidget.id);
-
-        // console.log('Changing height');
-        // const mainArea = document.querySelector('.jp-Notebook') as HTMLElement;
-        // mainArea.style.marginTop = '50px'; // Adjust this value to push the main area down
-
-        notebookSelector.addOptions();
-      }
+        });
     });
-
-    palette.addItem({ command: 'sidebar-cluster:open', category: 'Extension' });
   }
 };
 
