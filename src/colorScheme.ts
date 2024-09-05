eb8ba85dacebb506dcd01f9d8c34f5685a79e269
mchami <chamim@ethz.ch>
2024-09-05 20:28:54 +0200

    Now we are using colorScheme order

--- src/colorScheme.ts
+++ src/colorScheme.ts
@@ -8,17 +8,17 @@ type ColorScheme = {
 
 // Create the color scheme mapping
 const colorScheme: ColorScheme = {
-    "Data Extraction": "#4e79a7",  // Example class names, replace with actual class names
-    "Visualization": "#59a14f",
-    "Model Evaluation": "#9c755f",
     "Imports and Environment": "#f28e2b",
+    "Data Extraction": "#4e79a7",
+    "Data Transform": "#b07aa1",
     "Exploratory Data Analysis": "#edc948",
-    // "Model Interpretation": "#bab0ac",
+    "Visualization": "#59a14f",
+    "Model Training": "#ff9da7",
+    "Model Evaluation": "#9c755f",
     "Data Export": "#e15759",
+    // "Model Interpretation": "#bab0ac",
     // "Hyperparam Tuning": "#b07aa1",
     // "Debug": "#76b7b2",
-    "Model Training": "#ff9da7",
-    "Data Transform": "#b07aa1",
 };
 
 export default colorScheme;
