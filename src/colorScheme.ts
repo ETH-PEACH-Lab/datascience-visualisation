fe88ff51543e7896bd0111078f18cee82a036b62
mchami <chamim@ethz.ch>
2024-10-18 17:33:29 +0200

    colorscheme cleaned

--- src/colorScheme.ts
+++ src/colorScheme.ts
@@ -1,25 +1,18 @@
-/* eslint-disable @typescript-eslint/quotes */
-// colorScheme.ts
-
-// Define the type for a color scheme
+// Defines a color scheme where specific stages are mapped to a corresponding color
 type ColorScheme = {
   [key: string]: string;
 };
 
-// Create the color scheme mapping
+// Color scheme for different stages in data workflows
 const colorScheme: ColorScheme = {
-    "Imports and Environment": "#f28e2b",
-    "Data Extraction": "#4e79a7",
-    "Data Transform": "#b07aa1",
-    "Exploratory Data Analysis": "#edc948",
-    "Visualization": "#59a14f",
-    "Model Training": "#ff9da7",
-    "Model Evaluation": "#9c755f",
-    "Data Export": "#e15759",
-    // "Model Interpretation": "#bab0ac",
-    // "Hyperparam Tuning": "#b07aa1",
-    // "Debug": "#76b7b2",
+  "Imports and Environment": "#f28e2b",
+  "Data Extraction": "#4e79a7",
+  "Data Transform": "#b07aa1",
+  "Exploratory Data Analysis": "#edc948",
+  "Visualization": "#59a14f",
+  "Model Training": "#ff9da7",
+  "Model Evaluation": "#9c755f",
+  "Data Export": "#e15759",
 };
 
 export default colorScheme;
-  
